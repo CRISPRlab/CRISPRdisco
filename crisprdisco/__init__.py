34a5b748c6317c06b8c1bbe7d96fabb679c9111e
fstrklab <brinerallie@gmail.com>
2017-12-18 13:44:59 -0500

    fixing Dockerfile

---
+++ crisprdisco/__init__.py
@@ -0,0 +1,8 @@
+import casPROTidentification
+#Analyze_Results_plots_and_summary.py
+import generateMasterTbl
+import getCRISPRrepeats
+
+from crisprdisco.cli import script
+
+__version__=cli.script.__version__
--- disco.sh
+++ disco.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-time docker run -it --rm -v "/mnt":"/mnt" -v "$PWD":/home/crisprlab -e "CL_UID=$UID" crisprlab/crisprdisco disco $@
+time docker run -it --rm -v "/mnt":"/mnt" -v "$PWD":/home/crisprlab -v "test":"/opt/app/test" -e "CL_UID=$UID" crisprlab/crisprdisco disco $@
