b0c82d16c11d78ed8e5505ff90fb8f5d2c4aedbf
James R. Henriksen <jhenriksen@agbiome.com>
2018-01-26 15:51:53 -0500

    cosmetic changes to test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+set -e
 
-echo "Testing CLI"
+echo "___________Testing CLI (usage should print)___________"
 time ./disco.sh --help
 
-echo "Testing CRISPRdisco test run"
+echo "___________Testing CRISPRdisco test run___________"
 time ./disco.sh --workingdir test/temp_test_output --outdir test/test_output --refset typing --date 2017-12-20 test/test_infile.csv
 
-echo "Replication of CRISPRdisco output (differences)"
-diff test/test_output test/known_results
+echo "___________Replication of CRISPRdisco output (differences)___________"
+diff test/known_results test/test_output 
 
 rm -Rf test/test_output/*.* 
+
+echo "___________Tests pass___________"
