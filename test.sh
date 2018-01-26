34b8684e4cf547829a8152fb06f9213dba901911
James R. Henriksen <jhenriksen@agbiome.com>
2018-01-26 14:56:12 -0500

    fix testing system

--- test.sh
+++ test.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 echo "Testing CLI"
-#time docker run -it --rm crisprdisco disco --help
 time ./disco.sh --help
 
 echo "Testing CRISPRdisco test run"
-time ./disco.sh --workingdir test/temp_test_output --outdir test/test_output --refset typing --temp keep test/test_infile.csv
+time ./disco.sh --workingdir test/temp_test_output --outdir test/test_output --refset typing --date 2017-12-20 test/test_infile.csv
 
+echo "Replication of CRISPRdisco output (differences)"
+diff test/test_output test/known_results
+
+rm -Rf test/test_output/*.* 
