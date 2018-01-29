5aa3a69242ab588766aba04b2566c5ff636f1df5
James R. Henriksen <jhenriksen@agbiome.com>
2018-01-29 16:17:25 -0500

    remove duplicate timing

--- test.sh
+++ test.sh
@@ -2,10 +2,10 @@
 set -e
 
 echo "___________Testing CLI (usage should print)___________"
-time ./disco.sh --help
+./disco.sh --help
 
 echo "___________Testing CRISPRdisco test run___________"
-time ./disco.sh --workingdir test/temp_test_output --outdir test/test_output --refset typing --date 2017-12-20 test/test_infile.csv
+./disco.sh --workingdir test/temp_test_output --outdir test/test_output --refset typing --date 2017-12-20 test/test_infile.csv
 
 echo "___________Replication of CRISPRdisco output (differences)___________"
 diff test/known_results test/test_output 
